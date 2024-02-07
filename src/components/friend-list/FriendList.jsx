e55d82b3d086420212c29d27a4aa5158bdef4b5e
Vasyl-Djachuk <145469812+Vasyl-Djachuk@users.noreply.github.com>
2024-02-07 16:35:20 +0200

    Initial commit

---
+++ src/components/friend-list/FriendList.jsx
@@ -0,0 +1,18 @@
+import { FriendListItem } from './FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend}>
+      {friends.map(friend => (
+        <li className={css.items} key={friend.id}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
